bff6839afd42ac9dde00b69834f0d69a66ff2dbc
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-09-27 21:04:41 +0300

    get book data in viewmodel

--- app/src/main/java/ps/room/com/BookViewModel.kt
+++ app/src/main/java/ps/room/com/BookViewModel.kt
@@ -2,16 +2,19 @@ package ps.room.com
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
 class BookViewModel(application: Application) : AndroidViewModel(application) {
 
     private val bookDao: BookDao
+    val allBooks: LiveData<List<Book>>
 
     init {
         val bookDb = BookRoomDatabase.getDatabase(application)
         bookDao = bookDb!!.bookDao()
+        allBooks = bookDao.allBooks
     }
 
     fun insert(book: Book) {
